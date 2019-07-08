c9aee929d96856ca62f13fcc1fcd4fdb3e0c2984
enkodellc <keith@keithfimreite.com>
2019-07-08 10:43:48 -0700

    Updates for AuthorizeView, Clean up code

---
+++ src/BlazorBoilerplate.Client/Services/Implementations/IdentityAuthenticationStateProvider.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using BlazorBoilerplate.Client.Services.Contracts;
+using BlazorBoilerplate.Shared;
+
+namespace BlazorBoilerplate.Client.States
+{
+    public class IdentityAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private UserInfo _userInfoCache;
+        private readonly IAuthorizeApi _authorizeApi;
+
+        public IdentityAuthenticationStateProvider(IAuthorizeApi authorizeApi)
+        {
+            this._authorizeApi = authorizeApi;
+        }
+
+        public async Task<bool> IsLoggedIn()
+        {
+            try
+            {
+                var userInfo = await GetUserInfo();
+                return userInfo != null;
+            }
+            catch (HttpRequestException)
+            {
+                return false;
+            }
+        }
+
+        public async Task Login(LoginParameters loginParameters)
+        {
+            await _authorizeApi.Login(loginParameters);
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public async Task Register(RegisterParameters registerParameters)
+        {
+            await _authorizeApi.Register(registerParameters);
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public async Task Logout()
+        {
+            await _authorizeApi.Logout();
+            _userInfoCache = null;
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public async Task<UserInfo> GetUserInfo()
+        {
+            if (_userInfoCache != null && _userInfoCache.IsAuthenticated) return _userInfoCache;
+            _userInfoCache = await _authorizeApi.GetUserInfo();
+            return _userInfoCache;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+            try
+            {
+                var userInfo = await GetUserInfo();
+                if (userInfo.IsAuthenticated)
+                {
+                    var claims = new[] { new Claim(ClaimTypes.Name, _userInfoCache.Username) }.Concat(_userInfoCache.ExposedClaims.Select(c => new Claim(c.Key, c.Value)));
+                    identity = new ClaimsIdentity(claims, "Server authentication");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Request failed:" + ex.ToString());
+            }
+
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+        }
+    }
+}
