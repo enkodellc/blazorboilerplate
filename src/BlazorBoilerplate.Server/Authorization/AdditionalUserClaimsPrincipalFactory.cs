08df9e4a9dc02abf258ceeb27b01b6facabed570
enkodellc <keith@keithfimreite.com>
2019-07-11 14:46:07 -0700

    Screenshots, @attribute[Authorize]

--- src/BlazorBoilerplate.Server/Authorization/AdditionalUserClaimsPrincipalFactory.cs
+++ src/BlazorBoilerplate.Server/Authorization/AdditionalUserClaimsPrincipalFactory.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 using BlazorBoilerplate.Server.Models;
 using Microsoft.AspNetCore.Identity;
