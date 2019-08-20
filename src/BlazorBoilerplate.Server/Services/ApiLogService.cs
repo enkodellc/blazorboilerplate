8cf36d4d80255efa77b330973bb003d4c4972f13
Keith <keith@keithfimreite.com>
2019-08-20 01:53:01 -0700

    Update ApiLogService.cs

--- src/BlazorBoilerplate.Server/Services/ApiLogService.cs
+++ src/BlazorBoilerplate.Server/Services/ApiLogService.cs
@@ -26,13 +26,13 @@ namespace BlazorBoilerplate.Server.Services
             // Calling Log from the API Middlware results in a disposed ApplicationDBContext. This is here to build a DB Context for logging API Calls
             // If you have a better solution please let me know.
             _optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            if (Convert.ToBoolean(configuration["Authentication:UseSqlServer"] ?? "false"))
+            if (Convert.ToBoolean(Configuration["Authentication:UseSqlServer"] ?? "false"))
             {
-                _optionsBuilder.UseSqlite($"Filename={configuration.GetConnectionString("SqlLiteConnectionFileName")}");  // Sql Lite / file database
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")); //SQL Server Database                    
             }
             else
             {
-                _optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection")); //SQL Server Database
+                options.UseSqlite($"Filename={Configuration.GetConnectionString("SqlLiteConnectionFileName")}");  // Sql Lite / file database
             }
         }
 
