bc691c17d062fe3533e0b485b0a9484cb48fa391
Roundedcube <supportservices@roundedcube.com>
2020-01-30 12:46:19 -0600

    SSB auth cookies

--- src/BlazorBoilerplate.CommonUI/Services/Implementations/AuthorizeApi.cs
+++ src/BlazorBoilerplate.CommonUI/Services/Implementations/AuthorizeApi.cs
@@ -6,26 +6,85 @@ using Newtonsoft.Json;
 using BlazorBoilerplate.CommonUI.Services.Contracts;
 using BlazorBoilerplate.Shared.Dto;
 using System.Collections.Generic;
+using Microsoft.JSInterop;
+using System.Linq;
+using System.Net;
 
 namespace BlazorBoilerplate.CommonUI.Services.Implementations
 {
     public class AuthorizeApi : IAuthorizeApi
     {
         private readonly HttpClient _httpClient;
+        private readonly NavigationManager _navigationManager;
+        private readonly IJSRuntime _jsRuntime;
 
-        public AuthorizeApi(HttpClient httpClient)
+        public AuthorizeApi(NavigationManager navigationManager, HttpClient httpClient, IJSRuntime jsRuntime)
         {
-            _httpClient = httpClient;
+            _navigationManager = navigationManager;
+            _httpClient = httpClient; 
+            _jsRuntime = jsRuntime;
         }
 
         public async Task<ApiResponseDto> Login(LoginDto loginParameters)
         {
-            return await _httpClient.PostJsonAsync<ApiResponseDto>("api/Account/Login", loginParameters);
+            ApiResponseDto resp;
+
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "api/Account/Login");
+            httpRequestMessage.Content = new StringContent(JsonConvert.SerializeObject(loginParameters));
+            httpRequestMessage.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            using (var response = await _httpClient.SendAsync(httpRequestMessage))
+            {
+                response.EnsureSuccessStatusCode();
+
+#if ServerSideBlazor
+
+                if (response.Headers.TryGetValues("Set-Cookie", out var cookieEntries))
+                {
+                    var uri = response.RequestMessage.RequestUri;
+                    var cookieContainer = new CookieContainer();
+
+                    foreach (var cookieEntry in cookieEntries)
+                    {
+                        cookieContainer.SetCookies(uri, cookieEntry);
+                    }
+
+                    var cookies = cookieContainer.GetCookies(uri).Cast<Cookie>();
+
+                    foreach (var cookie in cookies)
+                    {
+                       await _jsRuntime.InvokeVoidAsync("jsInterops.setCookie", cookie.ToString());
+                    }
+                }
+#endif
+
+                var content = await response.Content.ReadAsStringAsync();
+                resp = JsonConvert.DeserializeObject<ApiResponseDto>(content);
+            }
+
+            return resp;
         }
 
         public async Task<ApiResponseDto> Logout()
         {
-            return await _httpClient.PostJsonAsync<ApiResponseDto>("api/Account/Logout", null);
+            var cookies = _httpClient.DefaultRequestHeaders.GetValues("Cookie").ToList();
+
+            var resp = await _httpClient.PostJsonAsync<ApiResponseDto>("api/Account/Logout", null);
+
+#if ServerSideBlazor
+            if (resp.StatusCode == 200 && cookies.Any())
+            {
+                _httpClient.DefaultRequestHeaders.Remove("Cookie");
+
+                foreach (var cookie in cookies[0].Split(';'))
+                {
+                    var cookieParts = cookie.Split('=');
+                    await _jsRuntime.InvokeVoidAsync("jsInterops.removeCookie", cookieParts[0]);
+                }
+            }
+#endif
+
+            return resp;
         }
 
         public async Task<ApiResponseDto> Create(RegisterDto registerParameters)
