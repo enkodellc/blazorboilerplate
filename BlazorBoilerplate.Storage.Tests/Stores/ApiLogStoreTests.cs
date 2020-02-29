c72ee4c45ba4522195f3b17fc338e8cf8c3a5e8a
Daniel Bunting <daniel.bunting93@gmail.com>
2020-02-29 23:13:04 +0000

    Adds initial manager test, and skeleton for a store test.

---
+++ BlazorBoilerplate.Storage.Tests/Stores/ApiLogStoreTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBoilerplate.Storage.Tests.Stores
+{
+    [TestFixture]
+    class ApiLogStoreTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Put in an actual DB instance, confirm what is returned after we seed it for the tests?
+        }
+    }
+}
