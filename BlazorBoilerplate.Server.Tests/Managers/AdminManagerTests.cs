e5c21e6609848cc29048347ce81d81483281b072
Daniel Bunting <daniel.bunting93@gmail.com>
2020-02-29 23:31:24 +0000

    Adds the Manager Test stubs.

---
+++ BlazorBoilerplate.Server.Tests/Managers/AdminManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using BlazorBoilerplate.Shared.DataModels;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBoilerplate.Server.Tests.Managers
+{
+    [TestFixture]
+    class AdminManagerTests
+    {
+        private Mock<UserManager<ApplicationUser>> _userManager;
+        private Mock<RoleManager<IdentityRole<Guid>>> _roleManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var userStore = new Mock<IUserStore<ApplicationUser>>();
+            var roleStore = new Mock<IRoleStore<IdentityRole<Guid>>>();
+
+            var roles = new List<IRoleValidator<IdentityRole<Guid>>>
+            {
+                new RoleValidator<IdentityRole<Guid>>()
+            };
+
+            _userManager = new Mock<UserManager<ApplicationUser>>(userStore.Object, null, null, null, null, null, null, null, null);
+            _roleManager = new Mock<RoleManager<IdentityRole<Guid>>>(roleStore.Object, roles, new UpperInvariantLookupNormalizer(), new IdentityErrorDescriber(), null);
+        }
+
+        [Test]
+        public void SetupWorked()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ BlazorBoilerplate.Server.Tests/Managers/ApiLogManagerTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBoilerplate.Server.Tests.Managers
+{
+    [TestFixture]
+    class ApiLogManagerTests
+    {
+    }
+}
