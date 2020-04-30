6968b8a22c430b3fbe583bab72107b1b3c5a48a2
Mobin <mobin.seven@live.com>
2020-04-30 16:27:22 +0430

    Persian Culture Support

--- src/Shared/BlazorBoilerplate.Localization/Settings.cs
+++ src/Shared/BlazorBoilerplate.Localization/Settings.cs
@@ -2,8 +2,8 @@
 {
     public static class Settings
     {
-        public static readonly string[] SupportedCultures = { "en-US", "it-IT" };
+        public static readonly string[] SupportedCultures = { "en-US", "it-IT", "fa-IR" };
 
-        public static readonly (string, string)[] SupportedCulturesWithName = new [] { ("English", "en-US"), ("Italiano", "it-IT") };
+        public static readonly (string, string)[] SupportedCulturesWithName = new[] { ("English", "en-US"), ("Italiano", "it-IT"), ("پارسی", "fa-IR") };
     }
 }
