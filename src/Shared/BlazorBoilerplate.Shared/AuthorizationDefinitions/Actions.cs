9de802b3bb98d92af12711afa3f7d80a05367385
GioviQ <giovanni@quarella.net>
2020-05-19 22:11:58 +0200

    tt revision

---
+++ src/Shared/BlazorBoilerplate.Shared/AuthorizationDefinitions/Actions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorBoilerplate.Shared.AuthorizationDefinitions
+{
+    [Flags]
+    public enum Actions
+    {
+        Create = 0,
+        Update = 1,
+        Read = 2,
+        Delete = 3
+    }
+}
