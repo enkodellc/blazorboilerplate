817a03de41ca8c5707df19a917d19e18c4784c52
GioviQ <giovanni@quarella.net>
2020-05-24 23:39:43 +0200

    migration queries fix

--- src/Server/BlazorBoilerplate.Storage/Migrations/TenantStoreDb/20200512074700_DefaultTenant.cs
+++ src/Server/BlazorBoilerplate.Storage/Migrations/TenantStoreDb/20200512074700_DefaultTenant.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using BlazorBoilerplate.Shared;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace BlazorBoilerplate.Storage.Migrations.TenantStoreDb
 {
@@ -16,12 +17,12 @@ namespace BlazorBoilerplate.Storage.Migrations.TenantStoreDb
                 columns: new[] { "Id", "ConnectionString", "Identifier", "Name" },
                 values: new object[] { "Master", null, "Master", "Master" });
 
-            migrationBuilder.Sql("UPDATE AspNetUserTokens SET TenantId='Master' WHERE TenantId='BlazorBoilerplate'");
-            migrationBuilder.Sql("UPDATE AspNetUserRoles SET TenantId='Master' WHERE TenantId='BlazorBoilerplate'");
-            migrationBuilder.Sql("UPDATE AspNetUserLogins SET TenantId='Master' WHERE TenantId='BlazorBoilerplate'");
-            migrationBuilder.Sql("UPDATE AspNetUserClaims SET TenantId='Master' WHERE TenantId='BlazorBoilerplate'");
-            migrationBuilder.Sql("UPDATE AspNetRoles SET TenantId='Master' WHERE TenantId='BlazorBoilerplate'");
-            migrationBuilder.Sql("UPDATE AspNetRoleClaims SET TenantId='Master' WHERE TenantId='BlazorBoilerplate'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserTokens')) UPDATE AspNetUserTokens SET TenantId='{Settings.DefaultTenantId}' WHERE TenantId='BlazorBoilerplate'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserRoles')) UPDATE AspNetUserRoles SET TenantId='{Settings.DefaultTenantId}' WHERE TenantId='BlazorBoilerplate'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserLogins')) UPDATE AspNetUserLogins SET TenantId='{Settings.DefaultTenantId}' WHERE TenantId='BlazorBoilerplate'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserClaims')) UPDATE AspNetUserClaims SET TenantId='{Settings.DefaultTenantId}' WHERE TenantId='BlazorBoilerplate'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetRoles')) UPDATE AspNetRoles SET TenantId='{Settings.DefaultTenantId}' WHERE TenantId='BlazorBoilerplate'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetRoleClaims')) UPDATE AspNetRoleClaims SET TenantId='{Settings.DefaultTenantId}' WHERE TenantId='BlazorBoilerplate'");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -36,12 +37,12 @@ namespace BlazorBoilerplate.Storage.Migrations.TenantStoreDb
                 columns: new[] { "Id", "ConnectionString", "Identifier", "Name" },
                 values: new object[] { "BlazorBoilerplate", null, "BlazorBoilerplate", "BlazorBoilerplate" });
 
-            migrationBuilder.Sql("UPDATE AspNetUserTokens SET TenantId='BlazorBoilerplate' WHERE TenantId='Master'");
-            migrationBuilder.Sql("UPDATE AspNetUserRoles SET TenantId='BlazorBoilerplate' WHERE TenantId='Master'");
-            migrationBuilder.Sql("UPDATE AspNetUserLogins SET TenantId='BlazorBoilerplate' WHERE TenantId='Master'");
-            migrationBuilder.Sql("UPDATE AspNetUserClaims SET TenantId='BlazorBoilerplate' WHERE TenantId='Master'");
-            migrationBuilder.Sql("UPDATE AspNetRoles SET TenantId='BlazorBoilerplate' WHERE TenantId='Master'");
-            migrationBuilder.Sql("UPDATE AspNetRoleClaims SET TenantId='BlazorBoilerplate' WHERE TenantId='Master'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserTokens')) UPDATE AspNetUserTokens SET TenantId='BlazorBoilerplate' WHERE TenantId='{Settings.DefaultTenantId}'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserRoles')) UPDATE AspNetUserRoles SET TenantId='BlazorBoilerplate' WHERE TenantId='{Settings.DefaultTenantId}'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserLogins')) UPDATE AspNetUserLogins SET TenantId='BlazorBoilerplate' WHERE TenantId='{Settings.DefaultTenantId}'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetUserClaims')) UPDATE AspNetUserClaims SET TenantId='BlazorBoilerplate' WHERE TenantId='{Settings.DefaultTenantId}'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetRoles')) UPDATE AspNetRoles SET TenantId='BlazorBoilerplate' WHERE TenantId='{Settings.DefaultTenantId}'");
+            migrationBuilder.Sql($"IF (EXISTS (SELECT * FROM sys.tables WHERE [name] = 'AspNetRoleClaims')) UPDATE AspNetRoleClaims SET TenantId='BlazorBoilerplate' WHERE TenantId='{Settings.DefaultTenantId}'");
         }
     }
 }
