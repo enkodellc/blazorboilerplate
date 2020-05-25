dcc2c558b9e32d18da12143cc7dd5a04aaefc798
GioviQ <giovanni@quarella.net>
2020-05-25 21:42:34 +0200

    PermissionRequirementHandler revision

--- src/Server/BlazorBoilerplate.Server/Authorization/PermissionRequirement .cs
+++ src/Server/BlazorBoilerplate.Server/Authorization/PermissionRequirement .cs
@@ -1,8 +1,4 @@
-﻿using BlazorBoilerplate.Infrastructure.Storage.DataModels;
-using BlazorBoilerplate.Storage;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Authorization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,44 +13,20 @@ namespace BlazorBoilerplate.Server.Authorization
 
         public string Permission { get; set; }
     }
-    public class PermissionRequirementHandler : AuthorizationHandler<PermissionRequirement>,  IAuthorizationRequirement
+
+    public class PermissionRequirementHandler : AuthorizationHandler<PermissionRequirement>, IAuthorizationRequirement
     {
-        private readonly ApplicationDbContext _context;
-
-        public PermissionRequirementHandler(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        protected override async Task HandleRequirementAsync(
-            AuthorizationHandlerContext context,
-            PermissionRequirement requirement)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
         {
             if (context.User == null)
-            {
-                return;
-            }
+                return Task.CompletedTask;
 
-            ApplicationUser user = await _context.Users.SingleOrDefaultAsync(u => u.UserName == context.User.Identity.Name);
-            if (user == null)
-            {
-                return;
-            }
-
-            var roleClaims = from ur in _context.UserRoles
-                             where ur.UserId == user.Id
-                             join r in _context.Roles on ur.RoleId equals r.Id
-                             join rc in _context.RoleClaims on r.Id equals rc.RoleId
-                             select rc;
-
-            if (await roleClaims.AnyAsync(c => c.ClaimValue == requirement.Permission))
-            {
+            if (context.User.Claims.Any(c => c.Type == "permission" && c.Value == requirement.Permission))
                 context.Succeed(requirement);
-            }
             else
-            {
                 context.Fail();
-            }
+
+            return Task.CompletedTask;
         }
     }
 }
