f73162cd86ed883830e590a04f96d4bf7e4acc1b
FreddyS96 <67339617+FreddyS96@users.noreply.github.com>
2020-06-23 23:09:46 +0200

    German localization

--- src/Shared/BlazorBoilerplate.Localization/Settings.cs
+++ src/Shared/BlazorBoilerplate.Localization/Settings.cs
@@ -2,8 +2,8 @@
 {
     public static class Settings
     {
-        public static readonly string[] SupportedCultures = { "en-US", "it-IT", "fa-IR" };
+        public static readonly string[] SupportedCultures = { "en-US", "de-DE", "it-IT", "fa-IR" };
 
-        public static readonly (string, string)[] SupportedCulturesWithName = new[] { ("English", "en-US"), ("Italiano", "it-IT"), ("پارسی", "fa-IR") };
+        public static readonly (string, string)[] SupportedCulturesWithName = new[] { ("English", "en-US"),("Deutsch", "de-DE"), ("Italiano", "it-IT"), ("پارسی", "fa-IR") };
     }
 }
