0b40f0415e39fe581c143983475650b1ecbac829
GioviQ <giovanni@quarella.net>
2020-10-08 22:15:19 +0200

    hiding of PasswordHash and SecurityStamp

--- src/Shared/BlazorBoilerplate.Infrastructure/Storage/DataModels/ApplicationUser.cs
+++ src/Shared/BlazorBoilerplate.Infrastructure/Storage/DataModels/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorBoilerplate.Infrastructure.AuthorizationDefinitions;
 using Finbuckle.MultiTenant;
 using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -20,12 +21,18 @@ namespace BlazorBoilerplate.Infrastructure.Storage.DataModels
         [MaxLength(64)]
         public string FullName { get; set; }
 
+        [JsonIgnore]
+        public override string PasswordHash { get; set; }
+
+        [JsonIgnore]
+        public override string SecurityStamp { get; set; }
+
         public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
 
         public ICollection<ApiLogItem> ApiLogItems { get; set; }
 
         public UserProfile Profile { get; set; }
 
-        public virtual ICollection<Message> Messages { get; set; }
+        public virtual ICollection<Message> Messages { get; set; }        
     }
 }
