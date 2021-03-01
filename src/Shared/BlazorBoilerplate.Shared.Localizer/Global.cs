65af3d112fc1d4f912abae72c243556613a640c0
GioviQ <giovanni@quarella.net>
2021-03-01 17:42:50 +0100

    GetCountryName

--- src/Shared/BlazorBoilerplate.Shared.Localizer/Global.cs
+++ src/Shared/BlazorBoilerplate.Shared.Localizer/Global.cs
@@ -1,6 +1,13 @@
-﻿namespace BlazorBoilerplate.Shared.Localizer
+﻿using System.Globalization;
+using System.Threading;
+
+namespace BlazorBoilerplate.Shared.Localizer
 {
     public class Global
     {
+        public static string GetCountryName(string countryCode)
+        {
+            return new RegionInfo(Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName + "-" + countryCode).NativeName.Replace("Itàlia", "Italia");
+        }
     }
 }
