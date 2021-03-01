1aa402b6ff1aab3895df510de12247aa636f8989
GioviQ <giovanni@quarella.net>
2021-03-01 17:51:58 +0100

    in urls it is better to use compressed Guid as paratemer

---
+++ src/Shared/BlazorBoilerplate.Shared/Extensions/GuidUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlazorBoilerplate.Shared.Extensions
+{
+    public static class GuidUtil
+    {
+        public static string ToCompressedString(this Guid guid)
+        {
+            return Convert.ToBase64String(guid.ToByteArray())
+                .Substring(0, 22)
+                .Replace('+', '-')
+                .Replace('/', '_');
+        }
+
+        public static Guid FromCompressedString(string compressedGuid)
+        {
+            string base64 = compressedGuid
+                .Replace('_', '/')
+                .Replace('-', '+')
+                + "==";
+
+            return new Guid(Convert.FromBase64String(base64));
+        }
+    }
+}
