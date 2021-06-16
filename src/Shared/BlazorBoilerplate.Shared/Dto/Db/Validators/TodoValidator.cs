f09b77517a7ba8f6050fc65d21cba088e32f1dd0
GioviQ <giovanni@quarella.net>
2021-06-16 18:42:27 +0200

    TodoValidator example

---
+++ src/Shared/BlazorBoilerplate.Shared/Dto/Db/Validators/TodoValidator.cs
@@ -0,0 +1,17 @@
+﻿using BlazorBoilerplate.Shared.Localizer;
+using BlazorBoilerplate.Shared.Validators;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace BlazorBoilerplate.Shared.Dto.Db.Validators
+{
+    public class TodoValidator : LocalizedAbstractValidator<Todo>
+    {
+        public TodoValidator(IStringLocalizer<Global> l) : base(l)
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .MaximumLength(128).WithName(L["Title"]);
+        }
+    }
+}
