cd2d5e9e16fe3581f4ea53e079e83d854b193383
GioviQ <giovanni@quarella.net>
2021-07-19 16:23:52 +0200

    wip

---
+++ src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Module.cs
@@ -0,0 +1,79 @@
+﻿using BlazorBoilerplate.Shared.Interfaces;
+using BlazorBoilerplate.Theme.MudBlazor.Services;
+using BlazorBoilerplate.Theme.MudBlazor.TagHelpers;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using System.Net.Http;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace BlazorBoilerplate.Theme.MudBlazor
+{
+    public class Module : IModule, ITheme
+    {
+        public Module()
+        {
+            RootComponentMapping = new RootComponentMapping(typeof(App), "app");
+        }
+
+        public RootComponentMapping RootComponentMapping { get; }
+
+        public string Name => "MudBlazor theme";
+
+        public string Description => "MudBlazor theme";
+
+        public int Order => 1;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ITagHelperComponent, ThemeTagHelperComponent>();
+            services.AddTransient<ITagHelperComponent, AppTagHelperComponent>();
+
+            services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 10000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            });
+
+            services.AddScoped<IViewNotifier, ViewNotifier>();
+        }
+
+        public void ConfigureWebAssemblyServices(IServiceCollection services)
+        {
+            services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 10000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            });
+
+            services.AddLoadingBar();
+
+            services.AddScoped<IViewNotifier, ViewNotifier>();
+
+            var sp = services.BuildServiceProvider();
+            
+            sp.GetRequiredService<HttpClient>().EnableIntercept(sp);
+        }
+
+        public void ConfigureWebAssemblyHost(WebAssemblyHost webAssemblyHost)
+        {
+            webAssemblyHost.UseLoadingBar();
+        }
+    }
+}
---
+++ src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/ViewNotifier.cs
@@ -0,0 +1,35 @@
+﻿using BlazorBoilerplate.Shared.Interfaces;
+using MudBlazor;
+
+namespace BlazorBoilerplate.Theme.MudBlazor.Services
+{
+    public class ViewNotifier : IViewNotifier
+    {
+        private readonly ISnackbar snackbar;
+        public ViewNotifier(ISnackbar snackbar)
+        {
+            this.snackbar = snackbar;
+        }
+        public void Show(string message, ViewNotifierType type, string title = null, string icon = null)
+        {
+            var snackType = Severity.Success;
+
+            switch (type)
+            {
+                case ViewNotifierType.Error:
+                    snackType = Severity.Error;
+                    break;
+                case ViewNotifierType.Warning:
+                    snackType = Severity.Warning;
+                    break;
+                case ViewNotifierType.Info:
+                    snackType = Severity.Info;
+                    break;
+            }
+
+            snackbar.Add(message, snackType, config => { 
+                config.Icon = icon;
+            });
+        }
+    }
+}
