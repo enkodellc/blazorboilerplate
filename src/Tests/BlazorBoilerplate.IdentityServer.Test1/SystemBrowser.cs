11c5a6c41cd3660aa3b1b4503d9aa45fabfa859e
GioviQ <giovanni@quarella.net>
2021-08-14 07:00:19 +0200

    new IdentityServer test

--- src/Tests/BlazorBoilerplate.IdentityServer.Test1/SystemBrowser.cs
+++ src/Tests/BlazorBoilerplate.IdentityServer.Test1/SystemBrowser.cs
@@ -4,11 +4,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Diagnostics;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
