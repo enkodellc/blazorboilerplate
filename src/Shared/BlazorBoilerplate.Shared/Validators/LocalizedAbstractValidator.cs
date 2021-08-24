32abed0359cce469ef5357ba7a5ea1b59c7ba377
GioviQ <giovanni@quarella.net>
2021-08-24 07:02:08 +0200

    new LocalizedAbstractValidator

--- src/Shared/BlazorBoilerplate.Shared/Validators/LocalizedAbstractValidator.cs
+++ src/Shared/BlazorBoilerplate.Shared/Validators/LocalizedAbstractValidator.cs
@@ -4,12 +4,20 @@ using Microsoft.Extensions.Localization;
 
 namespace BlazorBoilerplate.Shared.Validators
 {
-    public class LocalizedAbstractValidator<T> : AbstractValidator<T>
+    public class LocalizedAbstractValidator<T, S> : AbstractValidator<T>
     {
-        protected readonly IStringLocalizer<Global> L;
-        public LocalizedAbstractValidator(IStringLocalizer<Global> l)
+        protected readonly IStringLocalizer<S> L;
+        public LocalizedAbstractValidator(IStringLocalizer<S> l)
         {
             L = l;
+        }
+    }
+
+    public class LocalizedAbstractValidator<T> : LocalizedAbstractValidator<T, Global>
+    {
+        public LocalizedAbstractValidator(IStringLocalizer<Global> l) : base(l)
+        {
+
         }
     }
 }
