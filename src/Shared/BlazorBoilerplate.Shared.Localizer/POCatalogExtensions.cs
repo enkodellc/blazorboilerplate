ae89557e2cc1d1d03b97b81452cca567517dc858
GioviQ <giovanni@quarella.net>
2021-10-13 17:47:33 +0200

    EnableAuthenticatorInstructions

--- src/Shared/BlazorBoilerplate.Shared.Localizer/POCatalogExtensions.cs
+++ src/Shared/BlazorBoilerplate.Shared.Localizer/POCatalogExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Karambolo.PO;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 using System.Globalization;
 
 namespace BlazorBoilerplate.Shared.Localizer
@@ -6,5 +8,7 @@ namespace BlazorBoilerplate.Shared.Localizer
     public static class POCatalogExtensions
     {
         public static string GetCultureName(this POCatalog catalogo) => new CultureInfo(catalogo.Language.Replace("_", "-")).Name;
+
+        public static MarkupString ToMarkup(this LocalizedString localizedString) => (MarkupString)localizedString.Value;
     }
 }
