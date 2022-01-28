d6f638bab38e67e58213a30a3d4f0f75ee1ba7d2
Keith <keith@keithfimreite.com>
2022-01-28 12:02:59 -0700

    Add Pluralizer for Types
    Humanizer / Pluralizer for Types.

--- src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Shared/Components/ItemsTableBase.cs
+++ src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Shared/Components/ItemsTableBase.cs
@@ -2,6 +2,7 @@
 using BlazorBoilerplate.Shared.Models;
 using BlazorBoilerplate.UI.Base.Shared.Components;
 using MudBlazor;
+using Humanizer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,7 +43,7 @@ namespace BlazorBoilerplate.Theme.Material.Shared.Components
 
                 apiClient.ClearEntitiesCache();
 
-                var from = $"{typeof(T).Name}s";
+                var from = $"{typeof(T).Name}".Pluralize(true);
 
                 var prop = queryParameters?.GetType().GetProperties().SingleOrDefault(i => i.Name == "Filter");
 
