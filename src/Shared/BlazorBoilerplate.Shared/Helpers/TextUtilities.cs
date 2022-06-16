c483e1b5b59bb542efec58958e3d851d3e58f11d
GioviQ <giovanni@quarella.net>
2022-06-16 15:08:11 +0200

    TextUtilities

---
+++ src/Shared/BlazorBoilerplate.Shared/Helpers/TextUtilities.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace BlazorBoilerplate.Shared.Helpers
+{
+    public static class TextUtilities
+    {
+        public static string ToYesNo(bool? value, IStringLocalizer<object> L)
+        {
+            return value == true ? L["Yes"].ToString() : value == false ? L["No"].ToString() : string.Empty;
+        }
+    }
+}
