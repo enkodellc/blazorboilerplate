4856678e600eb68b5eff30657e1783b22059547d
GioviQ <giovanni@quarella.net>
2023-01-01 16:57:53 +0100

    hack to fix issue with Android

--- src/Maui/App.xaml.cs
+++ src/Maui/App.xaml.cs
@@ -6,12 +6,27 @@ namespace BlazorBoilerplateMaui;
 
 public partial class App : Application
 {
-	public App(ILocalizationApiClient localizationApiClient,
+    public App(ILocalizationApiClient localizationApiClient,
         ILocalizationProvider localizationProvider,
         ILogger<App> logger)
-	{
+    {
         InitializeComponent();
 
         MainPage = new LoadingPage(localizationApiClient, localizationProvider, logger);
     }
+
+#if ANDROID
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Destroying += (s, e) =>
+        {
+            Quit();
+        };
+
+        return window;
+    }
+#endif
+
 }
